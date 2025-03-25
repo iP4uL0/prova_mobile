44f3a6fffbc5eb71e98df684f8abbc7bef5b45d4
D3adSec <bruno.emedeiros99@gmail.com>
2025-03-25 13:26:11 -0300

    aula 25/03

--- components/Titulo/titulo.tsx
+++ components/Titulo/titulo.tsx
@@ -1,18 +1,12 @@
 import { View } from "react-native"
 import { Subtitulo, Titulo } from "./style"
 
-// Fabrincando um novo tipo para ser usado como base do meu
-// componente de texto estilizado
-
 type TitleProps = {
     texto : string,
     flag: boolean
 }
 
-// os : indicam que determinada coisa assumirá o 
-// tipo do que esta escrito na frente
-
-export default function Title({texto, flag} : TitleProps){
+export default function Title({texto, flag} : TitleProps ){
     return(
         <View>
             {flag == true ? 
