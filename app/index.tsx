786702a089699708ead2b5b5454b72a3b114c5b9
D3adSec <bruno.emedeiros99@gmail.com>
2025-03-31 11:22:15 -0300

    aula 01/04

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,54 @@ import styled from "styled-components/native";
 import Title from "../components/Titulo/titulo";
 import { useEffect, useState } from "react";
 import InputTexto from "@/components/Input/input";
+import { View } from "react-native";
 export default function App()
 {
 
-    const [email, setEmail] =  useState('')
+    const [email, setEmail] =  useState('example@example.com')
+    const [hasEmailError, setHasEmailError] = useState(false)
+
+    const [senha, setSenha] = useState('@Example123')
+    const [hasSenhaError, setHasSenhaError] = useState(false)
+
 
     useEffect(()=>{
-        console.log(email)
+
+        // Significa que a o texto que a pessoa digitar deve ser um email valido
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email))
+        {
+            // Se o texto não incluir o caractere @ e tiver menos que 3 carateres
+            // sera mostrado o campo como incorreto
+            setHasEmailError(true)
+        }
+        else{
+
+            // Quando a pessoa inserir um email valido, as bordas vermelhas vão sumir
+            setHasEmailError(false)
+        }
     },[email])
+
+    useEffect(()=>{
+        //  Usando expressão regular para diminuir a quantidade 
+        //  de condicionais para testar a senha
+        //  Esse Regex testa se a senha:
+        //  * Pelo menos 8 caracteres
+        //  * Pelo menos uma letra maiúscula
+        //  * Pelo menos um número
+        //  * Pelo menos um caractere especial (!@#$%^&*)
+        
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
+        if(!passwordRegex.test(senha))
+        {
+            setHasSenhaError(true)
+        }
+        else
+        {
+            setHasSenhaError(false)
+        }
+    },[senha])
 
     return(
      <Tela>
@@ -22,18 +62,28 @@ export default function App()
             flag={false}
         />
         <ContainerCampoTexto>
-           <InputTexto 
-                erro={true}
-                placeholder="Digite seu email..."
-                onChangeText={text => setEmail(text)}
-           />
-           <InputTexto 
-                erro={false}
-                placeholder="Digite sua senha..."
-           />
+           <View>
+            <InputTexto 
+                    erro={hasEmailError}
+                    placeholder="Digite seu email..."
+                    onChangeText={text => setEmail(text)}
+            />
+            {hasEmailError ? <TextErrorHint>E-mail invalido</TextErrorHint> : null}
+           </View>
+
+            <View>
+                <InputTexto 
+                        erro={hasSenhaError}
+                        placeholder="Digite sua senha..."
+                        onChangeText={text => setSenha(text)}
+                />
+                {hasSenhaError ? <TextErrorHint>Senha invalida</TextErrorHint> : null}
+           </View>
         </ContainerCampoTexto>
         <ContainerBotoes>
-            <Botao>
+            <Botao 
+                onPress={(()=> console.log('Cliking here'))}
+            >
                 <TextoBotao>Entrar</TextoBotao>
             </Botao>
             <Links>Cadastre-se</Links>
@@ -57,16 +107,22 @@ const ContainerBotoes = styled.View`
     gap: 20px;
 `
 const Botao = styled.Pressable`
-    background-color: #fff;
+    background-color: #2193F3;
     padding: 20px;
+    border-radius: 6px;
 `
 const TextoBotao = styled.Text`
     text-align: center;
     font-size: 24px;
-    font-weight: bold;
+    color: #fff;
 `
 const Links = styled.Text`
     text-align: center;
     color: #fff;
     font-size: 16px;
+`
+
+const TextErrorHint = styled.Text`
+    font-size: 16px;
+    color: #E63946;
 `
--- components/Input/style.tsx
+++ components/Input/style.tsx
@@ -8,6 +8,7 @@ export const CampoTexto = styled.TextInput<CampoDeTextoProps>`
     background-color: #fff;
     font-size: 18px;
     padding: 20px;
-    border: 4px solid ${({erro} : 
-    {erro: boolean}) => erro ? '#E03E59' : '#33415C'};
+    border-radius: 6px;
+    border: 2px solid ${({erro} : 
+    {erro: boolean}) => erro ? '#E63946' : '#33415C'};
 `
